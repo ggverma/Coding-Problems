57ccad554ec65b73c69284cee7bbd6068bf29344
gverma <gverma@ncsu.edu>
2016-11-30 19:08:39 -0500

    Added Problem statement

--- Odd-Even Linked Lists/OddEvenLL.java
+++ Odd-Even Linked Lists/OddEvenLL.java
@@ -70,3 +70,15 @@ public class OddEvenLL {
         return head;
     }
 }
+
+/*
+Problem Statement:
+
+Given a singly linked list, group all odd nodes together followed by the even nodes.
+Please note here we are talking about the node number and not the value in the nodes.
+
+Example:
+
+Input: 1 -> 2 -> 3 -> 4
+Output: 1 -> 3 -> 2-> 4
+*/
